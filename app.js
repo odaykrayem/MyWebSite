b40701e969d7c5581d8a2b004df4d487548de536
odaykrayem <0day.krayem0@gmail.com>
2022-07-21 03:49:32 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+//Scroll to top
+const scrolltop = document.querySelector("#scrolltop");
+scrolltop.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY >= 700) {
+        scrolltop.style.opacity = 1;
+    } else {
+        scrolltop.style.opacity = 0;
+
+    }
+});
+// Theme
+const themeToggle = document.querySelector(".checkbox");
+const body = document.querySelector("body");
+const darkmode = localStorage.getItem('dark');
+if (darkmode) {
+    body.classList.add("dark");
+    themeToggle.checked = true;
+}
+themeToggle.addEventListener("change", function() {
+    body.classList.toggle("dark");
+    if (body.classList.contains("dark")) {
+        localStorage.setItem("dark", "active");
+    } else {
+        localStorage.removeItem("dark");
+    }
+});
